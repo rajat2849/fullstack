20253ee1ccfd922bfe6ec434de9e7f40c947592c
dev/linux <dev@gmail.com>
2022-09-26 12:48:33 +0530

    full stack

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="title">
-          I am an Example App Running in the CloudSingh
+          I am an Example App Running in the RajatSingh
         </div>
       </div>
     );
